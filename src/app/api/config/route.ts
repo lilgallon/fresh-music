ab6edf1cb8460c18620382aca8f9b1fb921901f5
Lilian Gallon <lilian.gallon@4sh.fr>
2026-01-22 23:49:34 +0100

    fix docker not taking YOUTUBE_API_KEY into account

--- src/app/api/config/route.ts
+++ src/app/api/config/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
     return NextResponse.json({
         apiKey: process.env.YOUTUBE_API_KEY || process.env.NEXT_PUBLIC_YOUTUBE_API_KEY || "",
